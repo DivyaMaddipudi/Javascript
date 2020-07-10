d422e4b0ed4ac448f085a64794f381a1de11ef17
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-10 10:55:31 +0530

    java script started

---
+++ Javascript/2variables.js
@@ -0,0 +1,14 @@
+//fullName
+var fullName = "Satya Divya";
+
+var courseName = "Javascript";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+console.log(loggedCount);
+
+var paymentMode; //can be card, netbaking etc.,
+
+paymentMode ="Credit Card";
+console.log(paymentMode);
---
+++ Javascript/3basicSignup.js
@@ -0,0 +1,20 @@
+const uid = "abc123";
+
+var fullName = "SatyaDivya";
+var email = "divya.maddipudi1@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+//fullName = prompt("Enter your mane");
+
+console.log(uid);
+console.log("Full Name is:", fullName);
+
+console.log(`
+        Unique ID : ${uid}
+        Username : ${fullName}
+        email : ${email}
+
+`)
