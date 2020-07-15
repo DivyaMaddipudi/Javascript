4e819dae030ae9c3061f0fc82148f2d5b7b64389
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-15 13:52:34 +0530

    Modals added

---
+++ Javascript(FCS)/Day2/2020-07-15_1.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+/*
+These all are modals. Modals are those  popups, which don't allow you to interact
+with other part of webpages unless you have interacted with the modal.
+*/
+
+// Alert
+// alert("Hello..!")
+
+// Prompt
+//syntax: prompt("title", "default value"[optional])
+
+// let age = prompt("What is ur age")
+// console.log(age);
+
+// Confirm
+// return values: OK -> True, Cancel -> False
+confirm("Are you over 18 ?");
