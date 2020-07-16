af4757c5fac1c1c20886ec6ecac2f26d1ea12b1b
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-16 09:40:03 +0530

    assignment2 day3 added

--- Javascript(FCS)/Assignment Day3/assignment2.js
+++ Javascript(FCS)/Assignment Day3/assignment2.js
@@ -59,7 +59,69 @@ console.log(includesMessage);
 
 //Array methods
 
-var arr = ["hello", 1, ["welcome", 2]];
+var arr = ["hello", 1, "divya", 2, "welcome"];
 console.log(arr);
 
-//
+//Prints array elements to the browser.
+document.getElementById("arraySample").innerHTML = arr[0];
+
+//sort() - sorts array in ascending order.
+var sortArray = arr.sort();
+console.log(sortArray);
+
+//printing all array elements
+console.log("printing all the array elements")
+var arrLen = arr.length;
+text ="<ul>";
+for(i=0;i<arrLen;i++) {
+    text += "<li>" + arr[i] + "</li>";
+}
+text += "</ul>";
+document.getElementById('arraElements').innerHTML = text;
+
+//instanceof operator - returns true if an object is created by a given constructor.
+
+var instanceMsg = arr instanceof Array;
+console.log(instanceMsg);
+
+//toString() - converts array to string
+var toStringMsg = arr.toString();
+console.log(toStringMsg)
+
+
+//adds element to the starting
+var arr = ["hello", 1, "divya", 2, "welcome"];
+arr.unshift("java");
+console.log(arr);
+arr.shift(); //removes the first element.
+console.log(arr);
+
+
+//splice() - used to add new items to an array.
+//firstValue - position of elements to be added, secondValue - number of items to be removed after new elements added.
+ var arr = ["hello", 1, "divya", 2, "welcome"];
+arr.splice(1, 0, "javascript", "python" );
+console.log(arr);
+
+//removes the elements till index 2
+var arr = ["hello", 1, "divya", 2, "welcome"];
+arr.splice(0, 3);
+console.log(arr);
+
+
+//concat() - creates a new array by merging existing arrays.
+var arr1 = ["hello", 1, "divya", 2, "welcome"];
+var arr2 = ["java", "python", "javascript"];
+var newArray = arr1.concat(arr2);
+console.log(newArray);
+
+
+//slice() - method slices out a piece of an array into a new array.
+var arr = ["java", "python", "javascript", "jsp", "html"];
+var slicedArray = arr.slice(0, 2);
+console.log(slicedArray);
+
+
+
+
+
