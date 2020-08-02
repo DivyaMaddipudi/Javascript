97b0a9ca40d5f39fd17508e73b9a278d3e58bb55
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-02 22:43:20 +0530

    array functions in js added

---
+++ Javascript/16FillAndFilter.js
@@ -0,0 +1,9 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+
+// console.log(testArray.fill("d", 2, 5));
+
+const myNumber = [23, 24, 55, 66, 77, 87, 98];
+
+const result = myNumber.filter( (num) => num%2 === 0 && num>55);
+
+console.log(result);
---
+++ Javascript/17SliceAndSplice.js
@@ -0,0 +1,6 @@
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+
+console.log(users.slice(2, 5));
+
+users.splice(1, 2, "HI", "BYE", "Hello");
+console.log(users);
