6d4cffcfbdcffc55a53b2b903ee758007ddfebad
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-17 16:03:25 +0530

    while loop added

--- Javascript/19For.js
+++ Javascript/19For.js
@@ -11,9 +11,16 @@ const myStates = [
     "Assam"
 ];
 
-for(let i=0;i<myStates.length;i++) {
+// for(let i=0;i<myStates.length;i++) {
 
-    if(typeof myStates[i] !== "string") continue;
-    console.log(myStates[i]);   
+//     if(typeof myStates[i] !== "string") continue;
+//     console.log(myStates[i]);   
+// }
+
+let i = 0;
+
+while(i<myStates.length) {
+    console.log(myStates[i]);
+    i++;
 }
 
