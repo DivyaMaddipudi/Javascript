84db5134c900e5ba77bc977f815f178ca03a08c5
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-17 15:21:29 +0530

    Methods in Objects added

--- Javascript/18Objects.js
+++ Javascript/18Objects.js
@@ -3,13 +3,27 @@ var user = {
     age  : 21,
     role : "Admin",
     loginCount : 32,
-    facebookSignedIn :true
+    facebookSignedIn : true,
+    courseList: [],
+    buyCourse: function (courseName) {
+        this.courseList.push(courseName);
+    },
+
+    getCourseCount: function () {
+        return `${this.name} is enrolled in total of ${this.courseList.length} courses`;
+    },
 };
-console.log(user.facebookSignedIn);
+
+
+// console.log(user.facebookSignedIn);
 // console.log(user["role"]);
 
-console.log(user.loginCount);
-user.loginCount = 44;
-console.log(user.loginCount);
+// console.log(user.loginCount);
+// user.loginCount = 44;
+// console.log(user.loginCount);
 
-console.table(user);
+// console.table(user);
+
+console.log(user.getCourseCount());
+user.buyCourse("Javascript");
+console.log(user.getCourseCount());
