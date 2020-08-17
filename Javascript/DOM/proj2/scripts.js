780fab66efd8bf52a5c09097058674d6f784a042
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-17 22:50:17 +0530

    Project_2 MagicColor added

--- Javascript/DOM/proj2/scripts.js
+++ Javascript/DOM/proj2/scripts.js
@@ -1,16 +1,34 @@
 const red = document.querySelector(".red");
 const cyan = document.querySelector(".cyan");
-const viloet = document.querySelector(".viloet");
+const green = document.querySelector(".green");
 const orange = document.querySelector(".orange");
 const pink = document.querySelector(".pink");
 
 const center = document.querySelector(".center");
 
-// console.log(window.getComputedStyle(red).backgroundColor);
+console.log("orange", window.getComputedStyle(green).backgroundColor);
 
 const getBGColor = (selectedElement) => {
-    return window.getComputedStyle(selectedElement).backgroundColor
+    return window.getComputedStyle(selectedElement).backgroundColor;
 }
 
-console.log(getBGColor(orange));
+// var color = getBGColor(viloet);
+// viloet.addEventListener('mouseenter', () => {
+//     center.style.background = color;
+// })
+
+
+const magicColorChanger = (element, color) => {
+    return element.addEventListener('mouseenter', () => {
+        center.style.background = color;
+    });
+}
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+console.log(magicColorChanger(green, getBGColor(green)));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(pink, getBGColor(pink));
+
+
 
